95ea0eace276f18e0801d95adf0e26271ffa247c
duned <dune.desormeaux@mail.mcgill.ca>
2014-11-15 16:39:26 -0500

    Canny -> threshold -> findContours. Full of comments of things that were tried and failed, sorry.

--- thresholdTest.py
+++ thresholdTest.py
@@ -4,11 +4,29 @@ import sys
 
 impath = sys.argv[1]
 
-image = np.load(impath)
+image = cv2.imread(impath)
+#gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-binary, dst = cv2.threshold(image,80,255,0)
+edge = cv2.Canny(image,380, 405)
+
+binary, dst = cv2.threshold(edge,130,255,0)
+
+
+#fast = cv2.FastFeatureDetector()
+#kp = fast.detect(edge, None)
+# kp, des = orb.compute(edge, kp)
+
+#img2 = cv2.drawKeypoints(image,kp,color=(0,255,0), flags=0)
+
 
 #contours, h = cv2.FindContours(binary,1,2)
+#cv2.watershed(image,m)
 
-cv2.imshow("binary", dst)
+contours, h = cv2.findContours(dst,1,2)
+
+
+cv2.drawContours(image, contours,-1, (0,255,0))
+
+#cv2.imshow("binary", image)
+
 cv2.waitKey(0)
