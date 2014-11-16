b47dcddc6cfb528b32d4a3d6b85560a51a7e8513
Kelley Zhao <kelzha@github.com>
2014-11-15 21:46:35 -0800

    FACE DETECTION OPTIMIZED

--- crop.py
+++ crop.py
@@ -9,18 +9,18 @@ def cropp(imageFile, centerX, centerY, W, H):
     filepath,filename = os.path.split(imageFile)
     filtername,exts = os.path.splitext(filename)
     im = Image.open(imageFile)
-            
     #print im.size
 
     #Address blackboxing at bottom
-    bottom = int(centerY - H/2)
-    top = int(centerY + H/2)
+    bottom = int(centerY + H/2)
+    top = int(centerY - H/2)
 
-    if bottom < 0:
-    	top -= bottom
-    	bottom = 0
+    if bottom > im.size[1]:
+    	delta = bottom - im.size[1]
+        top -= delta
+    	bottom = im.size[1]
 
-    box = (int(centerX - W/2), bottom, int(centerX + W/2), top)
+    box = (int(centerX - W/2), top, int(centerX + W/2), bottom)
     print box
     cropped_im = im.crop(box)
-    cropped_im.save('./training_dataset_cropped/'+filtername+'_cropped'+".png",'PNG')
+    cropped_im.save(filepath + "_cropped/" +filtername+'_cropped'+".png",'PNG')
