97a7a604a016dd5de98cd88c86adbe8860b15f97
duned <dune.desormeaux@mail.mcgill.ca>
2014-11-20 21:42:46 -0500

    Optimized for bmp no PNG saving. Beware bmps not automatically grayscale

--- crop.py
+++ crop.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import glob
 import os
+import matplotlib.pyplot as plt
+import numpy as np
 
 files = glob.glob("./training_dataset/*.png")
 
@@ -8,7 +10,7 @@ files = glob.glob("./training_dataset/*.png")
 def cropp(imageFile, centerX, centerY, W, H):
     filepath,filename = os.path.split(imageFile)
     filtername,exts = os.path.splitext(filename)
-    im = Image.open(imageFile)
+    im = Image.open(imageFile).convert("L")
     #print im.size
     #Address blackboxing at bottom
     bottom = int(centerY + H/2.0)
@@ -19,8 +21,10 @@ def cropp(imageFile, centerX, centerY, W, H):
         top -= delta
     	bottom = im.size[1]
 
+
     box = (int(centerX - W/2.0), top, int(centerX + W/2.0), bottom)
     # print box
     cropped_im = im.crop(box)
+    # plt.imshow(cropped_im)
     return cropped_im
     #cropped_im.save("edited_" +filtername+".png",'PNG')
