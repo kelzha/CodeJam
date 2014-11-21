5bbdfb3e79a34523531c484fa750315316524bd6
duned <dune.desormeaux@mail.mcgill.ca>
2014-11-21 01:54:19 -0500

    Modified crop to accept rect and crop appropriately. Will also resize.

--- crop.py
+++ crop.py
@@ -4,17 +4,20 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-files = glob.glob("./training_dataset/*.png")
+#files = glob.glob("./training_dataset/*.png")
 
 
-def cropp(imageFile, centerX, centerY, W, H):
+def cropp(imageFile, rect):
     filepath,filename = os.path.split(imageFile)
     filtername,exts = os.path.splitext(filename)
     im = Image.open(imageFile).convert("L")
+
+    GoldenRto = 1.618
+
     #print im.size
     #Address blackboxing at bottom
-    bottom = int(centerY + H/2.0)
-    top = int(centerY - H/2.0)
+    bottom = rect[4]
+    top = rect[2]
 
     if bottom > im.size[1]:
     	delta = bottom - im.size[1]
@@ -22,9 +25,16 @@ def cropp(imageFile, centerX, centerY, W, H):
     	bottom = im.size[1]
 
 
-    box = (int(centerX - W/2.0), top, int(centerX + W/2.0), bottom)
+    box = (rect[0], top, rect[3], bottom)
     # print box
     cropped_im = im.crop(box)
+
+    fixedW = 160
+
+    fixedH = fixedW * GoldenRto
+
+
+
     # plt.imshow(cropped_im)
-    return cropped_im
+    return im.resize((fixedW,fixedH),Image.ANTIALIAS)
     #cropped_im.save("edited_" +filtername+".png",'PNG')
